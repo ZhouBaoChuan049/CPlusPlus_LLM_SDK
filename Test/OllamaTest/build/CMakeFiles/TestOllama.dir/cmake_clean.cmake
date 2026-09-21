c7c02eb248d6a71b0d466a43b4c2b1af2d2b849d
ZhouBaoChuan049 <3450014365@qq.com>
2026-09-21 17:16:45 +0800

    新增了Ollama第三方工具接入本地大模型,我目前的电脑只能支持最最轻量化的模型deepseek-r1:1.5b, 写了全量返回和流式返回两者,并做了测试.

---
+++ Test/OllamaTest/build/CMakeFiles/TestOllama.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestOllama.dir/OllamaSendTest.cc.o"
+  "CMakeFiles/TestOllama.dir/OllamaSendTest.cc.o.d"
+  "CMakeFiles/TestOllama.dir/home/konata/Project/CPlusPlus_LLM_SDK/Src/OllamaProvider.cc.o"
+  "CMakeFiles/TestOllama.dir/home/konata/Project/CPlusPlus_LLM_SDK/Src/OllamaProvider.cc.o.d"
+  "CMakeFiles/TestOllama.dir/home/konata/Project/CPlusPlus_LLM_SDK/Src/Util/LogModule.cc.o"
+  "CMakeFiles/TestOllama.dir/home/konata/Project/CPlusPlus_LLM_SDK/Src/Util/LogModule.cc.o.d"
+  "TestOllama"
+  "TestOllama.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestOllama.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
